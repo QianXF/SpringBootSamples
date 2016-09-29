c7a2f2f836c5a9f50f85f43e18a312574e2af340
qianxuefeng <849276016@qq.com>
2016-09-29 21:36:23 +0800

    添加注释

--- src/test/java/com/qianxuefeng/spring/service/HelloServiceTest.java
+++ src/test/java/com/qianxuefeng/spring/service/HelloServiceTest.java
@@ -2,7 +2,6 @@ package com.qianxuefeng.spring.service;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
